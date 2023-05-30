df1fbb1eee990f0fd92965eb8d0b646bc9a375d2
fultonoff <fultonkilebe7@gmail.com>
2023-05-30 12:29:45 +0200

    ✅added new team member

--- src/app/hoc/data.js
+++ src/app/hoc/data.js
@@ -1,131 +1,146 @@
-
 // Routes images
 
-import routeimg1 from '../../images/route/routeimg1.jpg'
-import routeimg2 from '../../images/route/routeimg2.jpg'
-import routeimg3 from '../../images/route/routeimg3.jpg'
-import routeimg5 from '../../images/route/routeimg5.jpg'
-
+import routeimg1 from "../../images/route/routeimg1.jpg";
+import routeimg2 from "../../images/route/routeimg2.jpg";
+import routeimg3 from "../../images/route/routeimg3.jpg";
+import routeimg5 from "../../images/route/routeimg5.jpg";
 
 // Whyus
 
-import why1 from '../../images/whyus/whyus1.jpg'
-import why2 from '../../images/whyus/whyus2.jpg'
-import why3 from '../../images/whyus/whyus3.jpg'
-import why4 from '../../images/whyus/whyus4.jpg'
-
-
+import why1 from "../../images/whyus/whyus1.jpg";
+import why2 from "../../images/whyus/whyus2.jpg";
+import why3 from "../../images/whyus/whyus3.jpg";
+import why4 from "../../images/whyus/whyus4.jpg";
 
 // Team members
 
-import teamuser1 from '../../images/team/teamuser1b.jpg'
-import teamuser2 from '../../images/team/teamuser2.jpeg'
-import teamuser3 from '../../images/team/teamuser3.jpeg'
-import teamuser4 from '../../images/team/teamuser4.jpeg'
-
-
-
+import teamuser1 from "../../images/team/teamuser1b.jpg";
+import teamuser2 from "../../images/team/teamuser2.jpeg";
+import teamuser3 from "../../images/team/teamuser3.jpeg";
+import teamuser4 from "../../images/team/teamuser4.jpeg";
+import teamuser5 from "../../images/team/teamuser5.jpeg";
 
 export const routes = [
-    {
-        id: 1,
-        title: 'ACCRA TO CAPE TOWN',
-        price: 9.112,
-        time: '13 hours 21 minutes',
-        url: '/',
-        image: routeimg1
-    },
-    {
-        id: 2,
-        title: 'LONDON TO SINGAPORE',
-        price: 9.112,
-        time: '12 hours 15 minutes',
-        url: '/',
-        image: routeimg2
-    },
-    {
-        id: 3,
-        title: 'LAGOS TO NEW YORK',
-        price: 9.112,
-        time: '10 hours 12 minutes',
-        url: '/',
-        image: routeimg3
-    },
-    {
-        id: 4,
-        title: 'SYDNEY TO DUBAI',
-        price: 9.112,
-        time: '14 hours 08 minutes',
-        url: '/',
-        image: routeimg5
-    },
-]
+  {
+    id: 1,
+    title: "ACCRA TO CAPE TOWN",
+    price: 9.112,
+    time: "13 hours 21 minutes",
+    url: "/",
+    image: routeimg1,
+  },
+  {
+    id: 2,
+    title: "LONDON TO SINGAPORE",
+    price: 9.112,
+    time: "12 hours 15 minutes",
+    url: "/",
+    image: routeimg2,
+  },
+  {
+    id: 3,
+    title: "LAGOS TO NEW YORK",
+    price: 9.112,
+    time: "10 hours 12 minutes",
+    url: "/",
+    image: routeimg3,
+  },
+  {
+    id: 4,
+    title: "SYDNEY TO DUBAI",
+    price: 9.112,
+    time: "14 hours 08 minutes",
+    url: "/",
+    image: routeimg5,
+  },
+];
 export const why = [
-    {
-        id: 1,
-        title: 'Convenient',
-        description: 'Booking a private jet enables the utmost convenience and flexibility scheduling the time of your flights to your choosing.',
-        image: why1
-    },
-    
-    {
-        id: 2,
-        title: 'Cost Effective',
-        description: 'By utilising a fleet of safe and cost-effective Citation Mustang jets, the price to fly private is now more affordable.',
-        image: why2
-    },
-    
-    {
-        id: 3,
-        title: 'Efficient',
-        description: 'Flying by private jet allows you to avoid the crowded terminals and fly point to point saving you hours every trip.',
-        image: why3
-    },
-    {
-        id: 4,
-        title: 'Safe',
-        description: 'We place our trust into the most professional and meticulous operators who put your safety first.',
-        image: why4
-    },
-    
-]
+  {
+    id: 1,
+    title: "Convenient",
+    description:
+      "Booking a private jet enables the utmost convenience and flexibility scheduling the time of your flights to your choosing.",
+    image: why1,
+  },
+
+  {
+    id: 2,
+    title: "Cost Effective",
+    description:
+      "By utilising a fleet of safe and cost-effective Citation Mustang jets, the price to fly private is now more affordable.",
+    image: why2,
+  },
+
+  {
+    id: 3,
+    title: "Efficient",
+    description:
+      "Flying by private jet allows you to avoid the crowded terminals and fly point to point saving you hours every trip.",
+    image: why3,
+  },
+  {
+    id: 4,
+    title: "Safe",
+    description:
+      "We place our trust into the most professional and meticulous operators who put your safety first.",
+    image: why4,
+  },
+];
 
 export const users = [
-    {
-        nom: 'Ahmat Tidjani Salim',
-        role: 'Ceo',
-        image: teamuser1,
-        id: 1,
-        facebook: 'https://www.facebook.com',
-        instagram: 'https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D',
-        description: 'Bonnie drives the technical strategy of the fly platform and brand.'
-    },
-    {
-        nom: 'Augustine Ewiah',
-        role: 'CMO',
-        image: teamuser2,
-        id: 2,
-        facebook: 'https://www.facebook.com',
-        instagram: 'https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D',
-        description: 'Bonnie drives the technical strategy of the fly platform and brand.'
-    },
-    {
-        nom: 'Joshua D. Goodrich',
-        role: 'Commercial director USA',
-        image: teamuser4,
-        id: 2,
-        facebook: 'https://www.facebook.com',
-        instagram: 'https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D',
-        description: 'Bonnie drives the technical strategy of the fly platform and brand.'
-    },
-    {
-        nom: 'Zynell Zuh',
-        role: 'Merketing director Africa',
-        image: teamuser3,
-        id: 3,
-        facebook: 'https://www.facebook.com',
-        instagram: 'https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D',
-        description: 'Bonnie drives the technical strategy of the fly platform and brand.'
-    },
-   
-]
+  {
+    nom: "Ahmat Tidjani Salim",
+    role: "Ceo",
+    image: teamuser1,
+    id: 1,
+    facebook: "https://www.facebook.com",
+    instagram:
+      "https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D",
+    description:
+      "Bonnie drives the technical strategy of the fly platform and brand.",
+  },
+  {
+    nom: "Augustine Ewiah",
+    role: "CMO",
+    image: teamuser2,
+    id: 2,
+    facebook: "https://www.facebook.com",
+    instagram:
+      "https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D",
+    description:
+      "Bonnie drives the technical strategy of the fly platform and brand.",
+  },
+  {
+    nom: "Joshua D. Goodrich",
+    role: "Commercial director USA",
+    image: teamuser4,
+    id: 2,
+    facebook: "https://www.facebook.com",
+    instagram:
+      "https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D",
+    description:
+      "Bonnie drives the technical strategy of the fly platform and brand.",
+  },
+  {
+    nom: "MC CALVIN",
+    role: "Team manager",
+    image: teamuser5,
+    id: 2,
+    facebook: "https://www.facebook.com",
+    instagram:
+      "https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D",
+    description:
+      "Bonnie drives the technical strategy of the fly platform and brand.",
+  },
+  {
+    nom: "Zynell Zuh",
+    role: "Marketing director Africa",
+    image: teamuser3,
+    id: 3,
+    facebook: "https://www.facebook.com",
+    instagram:
+      "https://www.instagram.com/flycojet/?igshid=OGQ5ZDc2ODk2ZA%3D%3D",
+    description:
+      "Bonnie drives the technical strategy of the fly platform and brand.",
+  },
+];
