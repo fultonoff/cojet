7e1982d85812ab3dba6c9ec1618a82f24d8060a6
fultonoff <fultonkilebe7@gmail.com>
2023-05-30 17:36:25 +0200

    ✅team updated

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -12,12 +12,12 @@ const TeamCard = ({data}) => {
             <Image className="w-full h-full object-cover" src={data.image} alt="Bonnie Avatar"/>
         </div>
         
-        <div className="">
+        <div className="w-full">
             <h3 className="text-xl font-Playfair">
                 {data.nom}
             </h3>
             <span className="text-gray-500 dark:text-gray-400">{data.role}</span>
-            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">{data.description}.</p>
+            {/* <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">{data.description}.</p> */}
             <ul className="flex space-x-4 sm:mt-0">
                 <li>
                     <Link href={data.instagram} target='_blank' className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
